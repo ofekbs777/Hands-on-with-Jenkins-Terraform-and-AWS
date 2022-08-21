4affdd520c7f7dd5200e91c871b22f7496d6f74e
ofekbs777 <zzxxcc050@gmail.com>
2022-08-21 12:21:02 -0400

    new

--- jobs/DeployReactApp.groovy
+++ jobs/DeployReactApp.groovy
@@ -4,7 +4,6 @@ pipelineJob("Deploy-React-App"){
         daysToKeep(5)
         numToKeep(20)
     }
-    concurrentBuild(allowConcurrentBuild = true)
     triggers {
         scm("* * * * *"){
             ignorePostCommitHooks(ignorePostCommitHooks = false)
